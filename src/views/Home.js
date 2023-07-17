5de550e4024030598392b032a30a8711f94342ba
marcoottaiano <marco.ottaiano00@gmail.com>
2023-07-17 11:18:33 +0200

    new sections

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <>
       <Header />
       <div className='background-container d-flex align-items-end'>
-        <div className='bg-overlay'></div>
+        <div className='bg-overlay' />
         <div className='home-title-container'>
           <h1 className='text-white'>Lorem ipsum dolor sit amet</h1>
           <hr className='divider' />
@@ -16,6 +16,13 @@ function Home() {
       </div>
       <div className='home-quote'>
         <span>"Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet"</span>
+      </div>
+      <div className='news-section'>
+        <div className='news-dark-overlay' />
+        <div className='news-text-container'>
+          <div className='text-white news-title'>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</div>
+          <div className='text-white news-subtitle'>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</div>
+        </div>
       </div>
       <Footer />
     </>
